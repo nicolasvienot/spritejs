e539b89ecf2c90cb478823eca34bf01e8ff0a927
BLOT Martial <martial.blot@outlook.fr>
2019-04-21 04:41:37 +0200

    opti

--- script.js
+++ script.js
@@ -10,16 +10,21 @@ img.onload = function() {
 // let ctx = canvas.getContext('2d');
 
 const scale = .5;
-const width = 110;
+const width = 93;
 const height = 120;
 const scaledWidth = scale * width;
 const scaledHeight = scale * height;
 
+function drawFrame(frameX, frameY, canvasX, canvasY){
+    ctx.drawImage(img, frameX*width, frameY*height,width, height,
+        canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
 function init() {
-    ctx.drawImage(img, 0, 0, width, height, 0, 0, scaledWidth, scaledHeight);
-    ctx.drawImage(img, width, 0, width, height, scaledWidth, 0, scaledWidth, scaledHeight);
-    ctx.drawImage(img, width*2, 0, width, height, scaledWidth*2, 0, scaledWidth, scaledHeight);
-    ctx.drawImage(img, width*3, 0, width, height, scaledWidth*3, 0, scaledWidth, scaledHeight);
-    ctx.drawImage(img, width*4, 0, width, height, scaledWidth*4, 0, scaledWidth, scaledHeight);
-    ctx.drawImage(img, width*5, 0, width, height, scaledWidth*5, 0, scaledWidth, scaledHeight);
+    drawFrame(0, 0, 0, 0);
+    drawFrame(1, 0, scaledWidth, 0);
+    drawFrame(2, 0, scaledWidth*2, 0);
+    drawFrame(3, 0, scaledWidth*3, 0);
+    drawFrame(4, 0, scaledWidth*4, 0);
+    drawFrame(5, 0, scaledWidth*5, 0);
 }
