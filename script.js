3ed789ee7b19c4c855533d5085ae018fa18dedb8
BLOT Martial <martial.blot@outlook.fr>
2019-04-21 04:31:55 +0200

    sonic etat

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ img.src = "./common/images/sonic.png";
 
 img.onload = function() {
     init();
-  };
-  
+};
 
-let canvas = document.getElementById("sonic");
-let ctx = canvas.getContext('2d');
+
+// let canvas = document.getElementById("sonic");
+// let ctx = canvas.getContext('2d');
 
 const scale = .5;
+const width = 110;
+const height = 120;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
 
 function init() {
-    ctx.drawImage(img, 0, 0, 90, 120, 0, 0, 90 * scale, 120 * scale);
+    ctx.drawImage(img, 0, 0, width, height, 0, 0, scaledWidth, scaledHeight);
+    ctx.drawImage(img, width, 0, width, height, scaledWidth, 0, scaledWidth, scaledHeight);
+    ctx.drawImage(img, width*2, 0, width, height, scaledWidth*2, 0, scaledWidth, scaledHeight);
+    ctx.drawImage(img, width*3, 0, width, height, scaledWidth*3, 0, scaledWidth, scaledHeight);
+    ctx.drawImage(img, width*4, 0, width, height, scaledWidth*4, 0, scaledWidth, scaledHeight);
+    ctx.drawImage(img, width*5, 0, width, height, scaledWidth*5, 0, scaledWidth, scaledHeight);
 }
