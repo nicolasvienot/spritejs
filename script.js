30c9e2a0c7ca672d52a50bc356f8a17ba3da19e9
BLOT Martial <martial.blot@outlook.fr>
2019-04-21 02:45:55 +0200

    function render

--- script.js
+++ script.js
@@ -3,9 +3,28 @@ sonicImage.src = "common/image/sonic.png";
 
 let canvas = document.getElementById("sonic");
 
+
 let sonic = sprite({
+
     context: canvas.getContext("2d"),
     width: 100,
     heigth: 100,
-    Image:sonicImage
-})
+    Image:sonicImage,
+    },
+    that.render = function () {
+        that.context.drawImage(
+            that.Image,
+            0,
+            0,
+            that.width,
+            that.height,
+            0,
+            0,
+            that.width,
+            that.height);
+        }
+        )
+
+sonic.render();
+    
+    
